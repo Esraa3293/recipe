cd38f1c422b061ba211f24e6443bc1a3223198eb
Esraa <esraahassan967@gmail.com>
2023-12-28 07:04:49 +0200

    images slider

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -44,64 +44,70 @@ class _HomeScreenState extends State<HomeScreen> {
           // ),
           body: Column(
         children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              CarouselSlider(
-                carouselController: carouselController,
-                options: CarouselOptions(
-                  height: 200.0,
-                  autoPlay: true,
-                  viewportFraction: .75,
-                  enlargeStrategy: CenterPageEnlargeStrategy.height,
-                  enlargeCenterPage: true,
-                  enlargeFactor: .3,
-                  onPageChanged: (index, _) {
-                    sliderIndex = index;
-                    setState(() {});
-                  },
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                CarouselSlider(
+                  carouselController: carouselController,
+                  options: CarouselOptions(
+                    height: 200.0,
+                    autoPlay: true,
+                    viewportFraction: .75,
+                    enlargeStrategy: CenterPageEnlargeStrategy.height,
+                    enlargeCenterPage: true,
+                    enlargeFactor: .3,
+                    onPageChanged: (index, _) {
+                      sliderIndex = index;
+                      setState(() {});
+                    },
+                  ),
+                  items: meals.map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration:
+                                const BoxDecoration(color: Colors.amber),
+                            child: Image.asset(
+                              meals[sliderIndex],
+                              scale: .8,
+                            ));
+                      },
+                    );
+                  }).toList(),
                 ),
-                items: meals.map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration: const BoxDecoration(color: Colors.amber),
-                          child: Image.asset(
-                            meals[sliderIndex],
-                            scale: .8,
-                          ));
-                    },
-                  );
-                }).toList(),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      sliderIndex--;
-                      if (sliderIndex == 0) {
-                        sliderIndex = meals.length - 1;
-                      }
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.arrow_back_ios),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      sliderIndex++;
-                      if (sliderIndex == meals.length) {
-                        sliderIndex = 0;
-                      }
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.arrow_forward_ios),
-                  ),
-                ],
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        sliderIndex--;
+                        if (sliderIndex == -1) {
+                          // sliderIndex = meals.length-1;
+                          sliderIndex = meals.indexOf(meals.last);
+                        }
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.arrow_back_ios),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        sliderIndex++;
+                        if (sliderIndex == meals.length) {
+                          // sliderIndex = 0;
+                          sliderIndex = meals.indexOf(meals.first);
+                        }
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.arrow_forward_ios),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
           DotsIndicator(
             dotsCount: meals.length,
