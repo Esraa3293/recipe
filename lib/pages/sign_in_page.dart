70de04291ea87c2634d5f0646c9395811d7f8772
Esraa <esraahassan967@gmail.com>
2024-02-20 18:30:02 +0200

    finish app

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -41,121 +41,118 @@ class _SignInPageState extends State<SignInPage> {
             fit: BoxFit.cover,
             width: double.infinity,
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              color: Colors.black38,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0).r,
-                child: Consumer<AppAuthProvider>(
-                  builder: (context, authProvider, child) {
-                    return Form(
-                      key: authProvider.formKey,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                    left: 50, right: 50, bottom: 25)
-                                .r,
-                            child: Image.asset(ImagesPath.baseHeader),
+          Container(
+            color: Colors.black38,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0).r,
+              child: Consumer<AppAuthProvider>(
+                builder: (context, authProvider, child) {
+                  return Form(
+                    key: authProvider.formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 50, right: 50, bottom: 25)
+                              .r,
+                          child: Image.asset(ImagesPath.baseHeader),
+                        ),
+                        Text(
+                          "Sign In",
+                          style: hellix18w700(),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                        TextFormField(
+                          style: const TextStyle(color: Colors.white),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "email is required";
+                            }
+                            if (!EmailValidator.validate(value)) {
+                              return "email not valid";
+                            }
+                            return null;
+                          },
+                          controller: authProvider.emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            labelText: "Email",
+                            prefixIcon: Icon(Icons.email_outlined),
                           ),
-                          Text(
-                            "Sign In",
-                            style: hellix18w700(),
-                            textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                        TextFormField(
+                          style: const TextStyle(color: Colors.white),
+                          validator: (value) {
+                            bool passwordValid = RegExp(
+                                r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+                                .hasMatch(value!);
+                            if (value.isEmpty) {
+                              return "password is required";
+                            } else if (!passwordValid) {
+                              return "password not valid";
+                            }
+                            return null;
+                          },
+                          controller: authProvider.passwordController,
+                          obscureText: authProvider.obsecureText,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            prefixIcon: const Icon(Icons.lock_outline),
+                            suffixIcon: InkWell(
+                                onTap: () {
+                                  authProvider.toggleObsecure();
+                                },
+                                child: Icon(authProvider.obsecureText
+                                    ? Icons.visibility_off
+                                    : Icons.visibility)),
                           ),
-                          SizedBox(
-                            height: 20.h,
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigation.push(
+                                context: context,
+                                page: const ForgotPasswordPage());
+                          },
+                          child: Text(
+                            "Forgot Password",
+                            style: hellix12w500()
+                                .copyWith(color: ColorsConst.titleColor),
+                            textAlign: TextAlign.end,
                           ),
-                          TextFormField(
-                            style: const TextStyle(color: Colors.white),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return "email is required";
-                              }
-                              if (!EmailValidator.validate(value)) {
-                                return "email not valid";
-                              }
-                              return null;
-                            },
-                            controller: authProvider.emailController,
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: const InputDecoration(
-                              labelText: "Email",
-                              prefixIcon: Icon(Icons.email_outlined),
+                        ),
+                        SizedBox(
+                          height: 30.h,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            await authProvider.signIn(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: ColorsConst.primaryColor,
+                            minimumSize: Size(20.w, 50.h),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.r),
                             ),
                           ),
-                          SizedBox(
-                            height: 20.h,
-                          ),
-                          TextFormField(
-                            style: const TextStyle(color: Colors.white),
-                            validator: (value) {
-                              bool passwordValid = RegExp(
-                                      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
-                                  .hasMatch(value!);
-                              if (value.isEmpty) {
-                                return "password is required";
-                              } else if (!passwordValid) {
-                                return "password not valid";
-                              }
-                              return null;
-                            },
-                            controller: authProvider.passwordController,
-                            obscureText: authProvider.obsecureText,
-                            decoration: InputDecoration(
-                              labelText: "Password",
-                              prefixIcon: const Icon(Icons.lock_outline),
-                              suffixIcon: InkWell(
-                                  onTap: () {
-                                    authProvider.toggleObsecure();
-                                  },
-                                  child: Icon(authProvider.obsecureText
-                                      ? Icons.visibility_off
-                                      : Icons.visibility)),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 15.h,
-                          ),
-                          InkWell(
-                            onTap: () {
-                              Navigation.push(
-                                  context: context,
-                                  page: const ForgotPasswordPage());
-                            },
-                            child: Text(
-                              "Forgot Password",
-                              style: hellix12w500()
-                                  .copyWith(color: ColorsConst.titleColor),
-                              textAlign: TextAlign.end,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 30.h,
-                          ),
-                          ElevatedButton(
-                            onPressed: () async {
-                              await authProvider.signIn(context);
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: ColorsConst.primaryColor,
-                              minimumSize: Size(20.w, 50.h),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.r),
-                              ),
-                            ),
-                            child: Text("Sign In",
-                                style: hellix16white()
-                                    .copyWith(fontWeight: FontWeight.w600)),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
+                          child: Text("Sign In",
+                              style: hellix16white()
+                                  .copyWith(fontWeight: FontWeight.w600)),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
           ),
